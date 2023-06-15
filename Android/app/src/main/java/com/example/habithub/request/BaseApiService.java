4009d94b5c9c3564f814114406ccab3eba9f07cb
IdleGoat <rafie.amandio@ui.ac.id>
2023-06-15 13:05:54 +0700

    Test

---
+++ Android/app/src/main/java/com/example/habithub/request/BaseApiService.java
@@ -0,0 +1,12 @@
+package com.example.habithub.request;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public class BaseApiService {
+
+
+}
--- Android/app/src/main/java/com/example/habithub/request/UtilsAPI.java
+++ Android/app/src/main/java/com/example/habithub/request/UtilsAPI.java
@@ -1,4 +1,9 @@
 package com.example.habithub.request;
 
 public class UtilsAPI {
+    public static final String BASE_URL_API = "http://10.0.2.2:8080/";
+
+    public static BaseApiService getApiService(){
+        return RetrofitClient.getClient(BASE_URL_API).create(BaseApiService.class);
+    }
 }
